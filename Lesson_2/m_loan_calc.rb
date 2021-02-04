27d0ee7f7c9e9bdaf8bfdc472aa8f6c2e0c126ff
Matt Register <mattregister@Matts-MacBook-Pro.local>
2021-02-04 13:53:54 -0500

    adding mortgage loan calculator and associated yml file

--- Lesson_2/m_loan_calc.rb
+++ Lesson_2/m_loan_calc.rb
@@ -1,10 +1,14 @@
+require 'yaml'
+require 'pry'
+
+MESSAGES = YAML.load_file('m_loan_calc.yml')
 
 def int_valid?(input)
-  input.is_a? Integer
+  input.to_i.to_s == input && input.include?("-") == false
 end
 
 def float_valid?(input)
-  input.is_a Float
+  input.to_f.to_s == input && input.include?("-") == false
 end
 
 def monthly_payment_calc(loan_am, int_rate,duration)
@@ -14,3 +18,37 @@ def monthly_payment_calc(loan_am, int_rate,duration)
   loan_am * int_and_principal_rate # returns final monthly payment
 end
 
+keep_going = true
+
+while keep_going
+  user_input = {loan_amount: '', apy: '', loan_term: ''}
+  puts MESSAGES['welcome']
+
+  loop do
+    puts MESSAGES['loan_amount_prompt']
+    user_input[:loan_amount] = gets.chomp
+    break if int_valid?(user_input[:loan_amount]) 
+    puts MESSAGES['error_message']
+  end
+
+  loop do
+    puts MESSAGES['apy_prompt']
+    user_input[:apy] = gets.chomp
+    break if float_valid?(user_input[:apy])
+    puts MESSAGES['float_error_message']
+  end
+
+  loop do
+    puts MESSAGES['loan_term_prompt']
+    user_input[:loan_term] = gets.chomp
+    break if int_valid?(user_input[:loan_term])
+    puts MESSAGES['error_message']
+  end
+
+  puts "Calculating...."
+  puts "Your estimated monthly payments is $#{format('%.2f',monthly_payment_calc(user_input[:loan_amount].to_i,user_input[:apy].to_f,user_input[:loan_term].to_i))}. Good luck paying that off!"
+  puts "Would you like to enter another loan? Press Y if yes"
+  gets.chomp == "y".downcase ? keep_going = true : keep_going = false
+end
+
+puts 'Have a wonderful day! Thanks for using our app.'
